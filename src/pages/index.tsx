b25d79660c3c007146d11b0e23edceb15ca34f5b
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-04-02 03:47:54 +0700

    feat: use nwerc example for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,58 @@
-const HomePage = () => {
-  return <div>HomePage</div>
+import {
+  sampleContest,
+  sampleProblems,
+  sampleScoreboard,
+  sampleTeams,
+} from '@/domjudge/data/sample'
+import { convertToICPCScoreboardData } from '@/domjudge/utils/DomjudgeUtil'
+import ICPCScoreboardTable from '@/icpc/components/elements/ICPCScoreboardTable'
+import ScoreboardData from '@/icpc/types/ScoreboardData'
+import { GetStaticProps } from 'next'
+import { NextSeo } from 'next-seo'
+import { FC } from 'react'
+
+type Props = {
+  data: ScoreboardData
+  lastUpdated: string
+}
+
+const HomePage: FC<Props> = ({
+  data: { problems, teams, contest },
+  lastUpdated,
+}) => {
+  return (
+    <>
+      <NextSeo
+        title={`${contest.name} Scoreboard - Competitive Programming Scoreboard`}
+        description="An example from ICPC Scoreboard with data taken from DOMJudge demoweb nwerce18"
+      />
+      <header>
+        <h1>{contest.name} Scoreboard</h1>
+        <p>Last updated: {lastUpdated}</p>
+      </header>
+      <main>
+        <ICPCScoreboardTable problems={problems} teams={teams} />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const problems = sampleProblems
+  const scoreboard = sampleScoreboard
+  const teams = sampleTeams
+  const contest = sampleContest
+  const lastUpdated = new Date().toLocaleString('id-ID', {
+    timeZone: 'Asia/Jakarta',
+    hour12: true,
+  })
+
+  return {
+    props: {
+      data: convertToICPCScoreboardData(scoreboard, teams, problems, contest),
+      lastUpdated,
+    },
+  }
 }
 
 export default HomePage
